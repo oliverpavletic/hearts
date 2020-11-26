692dc2f3f63b3bb24c51d273fa7bcc80b81c2eb1
oliverpavletic <oliver.pavletic@gmail.com>
2020-11-26 10:34:03 -0500

    only use betterBotStrategy

--- server/src/game/bot_strategies/botStrategyManager.ts
+++ server/src/game/bot_strategies/botStrategyManager.ts
@@ -1,10 +1,10 @@
 import { getRandomInt } from '../rand'
 import { BotStrategy } from './botStrategy'
 import { BetterBotStrategy } from './betterBotStrategy'
-import { RandomBotStrategy } from './randomBotStrategy'
-import { SimpleBotStrategy } from './simpleBotStrategy'
+// import { RandomBotStrategy } from './randomBotStrategy'
+// import { SimpleBotStrategy } from './simpleBotStrategy'
 
-const strategies: BotStrategy[] = [new BetterBotStrategy(), new SimpleBotStrategy(), new RandomBotStrategy()]
+const strategies: BotStrategy[] = [new BetterBotStrategy()]
 
 export const getRandomStrategy = (): BotStrategy => {
   return strategies[getRandomInt(strategies.length)]
